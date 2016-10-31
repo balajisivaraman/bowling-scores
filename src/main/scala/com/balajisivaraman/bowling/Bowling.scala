71baebfa6df6fee1deb0ff314958c53fb4ae6a9c
Balaji Sivaraman <balaji@balajisivaraman.com>
2016-10-31 21:16:05 +0530

    Minor cleanup to separate out final bonus rolls

--- src/main/scala/com/balajisivaraman/bowling/Bowling.scala
+++ src/main/scala/com/balajisivaraman/bowling/Bowling.scala
@@ -4,18 +4,17 @@ sealed trait Frame
 case object Strike extends Frame
 case class Spare(first: Integer) extends Frame
 case class Roll(first: Integer, second: Integer) extends Frame
-case class Bonus(score: Integer) extends Frame
+
+case class Bonus(score: Integer)
+//This ideally needs to be 10 frames, but we can't get that safety for now.
+case class Game(frames: List[Frame], bonusRolls: List[Bonus])
 
 object Bowling extends App {
-  //This ideally needs to be 10 frames, but we can't get that safety for now.
-  type Game = List[Frame]
-
   def computeFrameScores(game: Game): Integer = {
-    val mainScore = game.foldLeft(0)(_ + computeScore(_))
-    println(s"main score is $mainScore")
+    val mainScore = game.frames.foldLeft(0)(_ + computeScore(_))
     val bonusScore = computeBonusScores(game)
-    println(s"bonus score is $bonusScore")
-    mainScore + bonusScore
+    val bonusForFinalFrame = game.bonusRolls.foldLeft(0)(_ + _.score)
+    mainScore + bonusScore + bonusForFinalFrame
   }
 
   def computeScore(frame: Frame): Integer = {
@@ -23,20 +22,20 @@ object Bowling extends App {
       case Strike              => 10
       case Spare(f)            => 10
       case Roll(first, second) => first + second
-      case Bonus(_)            => 0
     }
   }
 
   def computeBonusScores(game: Game): Integer = {
+
     def loop(frames: List[Frame], acc: Integer): Integer = {
       frames match {
         case Nil                  => acc
         case Strike :: rest       => loop(rest, acc + computeBonusForStrike(rest))
         case Spare(first) :: rest => loop(rest, acc + computeBonusForSpare(rest))
         case Roll(_, _) :: rest   => loop(rest, acc)
-        case Bonus(_) :: rest     => loop(rest, acc)
       }
     }
+
     def computeBonusForStrike(frames: List[Frame]): Integer = {
       frames match {
         case Nil => 0
@@ -46,7 +45,6 @@ object Bowling extends App {
         case Strike :: Roll(first, _) :: rest => 10 + first
         case Spare(first) :: rest => 10
         case Roll(first, second) :: rest => first + second
-        case Bonus(b1) :: Bonus(b2) :: Nil => b1 + b2
       }
     }
 
@@ -56,21 +54,12 @@ object Bowling extends App {
         case Strike :: rest => 10
         case Spare(first) :: rest => first
         case Roll(first, second) :: rest => first
-        case Bonus(b1) :: Nil => b1
       }
     }
-    loop(game, 0)
+
+    loop(game.frames, 0)
   }
 
-
-  def computeFirstRoll(frame: Frame): Integer = {
-    frame match {
-      case Strike         => 10
-      case Spare(first)   => first
-      case Roll(first, _) => first
-    }
-  }
-
-  val game: Game = List(Roll(1,4), Roll(4,5), Spare(6), Spare(5), Strike, Roll(0,1), Spare(7), Spare(6), Strike, Spare(2), Bonus(6))
+  val game: Game = Game(List(Roll(1,4), Roll(4,5), Spare(6), Spare(5), Strike, Roll(0,1), Spare(7), Spare(6), Strike, Spare(2)), List(Bonus(6)))
   println(computeFrameScores(game))
 }
